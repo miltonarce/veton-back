c063b9ebe8733678af4e992c980cca956ef7700a
Milton Arce <miltonhernan.arce@gmail.com>
2020-07-14 01:14:31 -0300

    fix en cors

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -17,7 +17,7 @@ class CORS
     {
         
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
             ->header('Access-Control-Allow-Credentials', 'true');
